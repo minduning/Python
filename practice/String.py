f6b9977a53f7289373a0604f66fa2a0dce8ee000
minduning <conan20700@gmail.com>
2025-09-09 23:26:44 +0900

    Create String.py

---
+++ practice/String.py
@@ -0,0 +1,20 @@
+"string"
+'string'
+# 문자열이고 값을 변경할 수 없고, 순서도 못 바꾼다.
+
+my_str = "김민준"
+print(my_str)
+print(type(my_str))
+
+my_str = '민두닝'
+print(my_str)
+print(type(my_str))
+
+# 큰 따옴표나 작은 따옴표나 똑같이 결과가 나옴
+
+my_str = """김
+민
+준
+"""
+print(my_str)
+# """ ''' 따옴표 3개 쓰면 줄바꿈을 저장할 수 있음
