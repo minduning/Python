6577813f23f1f698d42b3446fad6ec32eb5b8bf3
minduning <conan20700@gmail.com>
2025-09-09 22:40:05 +0900

    Create print.py

---
+++ practice/print.py
@@ -0,0 +1,11 @@
+print('Hello world!')
+print(1)
+# print()라는 '문법' 약속이다.
+print([1, 2, 3])
+print(1, 2)
+print(1, 2, 3)
+'''
+띄어쓰기는 해도 되고 안해도 된다. 가독성을 따지자면 띄어쓰기 할 것.
+쉼표가 표시가 안되는 것이 볼만하다.
+'''
+print('My name is', 'MinJun')
