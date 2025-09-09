dbfadbfe369d7c5045bcc4ff44d3d5cfa7d52104
minduning <conan20700@gmail.com>
2025-09-09 23:05:43 +0900

    Create Type.py

---
+++ practice/Type.py
@@ -0,0 +1,19 @@
+my_int = 1
+my_float = 3.14
+
+print(type(my_int))
+print(type(my_float))
+
+print(1 + 2)
+print(3 + 3)
+
+print('Hello')
+print("Hello")
+# "와 '를 같이는 못 씀
+
+# Boolean 참과 거짓
+print(True)
+print(False)
+
+my_bool = True
+print(my_bool)
