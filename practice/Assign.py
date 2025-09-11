1122d20bf085135b9d4511956a00fbbe3fbbf7f6
minduning <conan20700@gmail.com>
2025-09-11 16:18:08 +0900

    Create Assign.py

---
+++ practice/Assign.py
@@ -0,0 +1,22 @@
+# Operator
+
+# Assign 할당 연산자
+# = += -= *= /=
+
+count = 0
+print(count)
+
+count = 1
+print(count)
+
+count = count + 1
+print(count)
+
+count += 1
+print(count)
+
+count = count - 1
+print(count)
+
+count -= 1
+print(count)
