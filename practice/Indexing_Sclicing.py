38767ef2a07a65efdf14e1b3f6ee4e1e59178ddd
minduning <conan20700@gmail.com>
2025-09-11 12:49:56 +0900

    Create Indexing_Sclicing.py

---
+++ practice/Indexing_Sclicing.py
@@ -0,0 +1,20 @@
+# Indexing
+
+animals = ['고양이', '하이에나', '코알라', '다람쥐']
+animals.append('바다코끼리')
+animals.append('스컹크')
+animals.append('아나콘다')
+
+print(animals)
+
+print(animals[3])
+print(animals[4])
+
+del animals[4]
+print(animals)
+
+animals.append('바다코끼리')
+print(animals)
+
+#Sclicing
+print(animals[1:3])
