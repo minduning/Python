cbc88a7f18ea4df77c4cb3971c6b1edd5209d936
minduning <conan20700@gmail.com>
2025-09-11 15:25:58 +0900

    Create Range.py

---
+++ practice/Range.py
@@ -0,0 +1,15 @@
+# range()
+
+print(range(3))
+print(type(range(3)))
+
+for n in [0, 1, 2]:
+    print(n)
+
+for n in range(0, 3):
+    print(n)
+
+for n in range(0, 100):
+    print(n)
+    
+#숫자를 하나만 넣으면 0부터 그 숫자까지 센다.
