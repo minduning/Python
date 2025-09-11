7b84bdd8453302e19c14974354191ce7e633e5cf
minduning <conan20700@gmail.com>
2025-09-11 14:58:08 +0900

    Create List_sort.py

---
+++ practice/List_sort.py
@@ -0,0 +1,18 @@
+# list.sort()
+
+animals = ['사자', '토끼', '아나콘다', '개미', '호랑이', '박쥐']
+
+animals.sort() # 가나다순으로 바뀜
+print(animals)
+
+animals.append('바다소')
+print(animals)
+
+print(animals.count('바다소'))
+
+print(animals)
+
+del animals[1]
+print(animals)
+
+print(len(animals))
