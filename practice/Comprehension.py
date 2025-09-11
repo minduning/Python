8d37d6c0d9d7628a41f97f2afeb837544e1eb0b2
minduning <conan20700@gmail.com>
2025-09-11 16:07:06 +0900

    Create Comprehension.py

---
+++ practice/Comprehension.py
@@ -0,0 +1,13 @@
+# Comprehension
+
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+odd_numbers = []
+
+for number in numbers:
+    if number % 2 == 1:
+        odd_numbers.append(number)
+
+
+print(odd_numbers)
+
+print([number for number in numbers if numbers if number % 2 ==1])
